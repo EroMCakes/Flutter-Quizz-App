23c3438a4b0575d6bb802759ce551825a21eb98e
EroMCakes <salah17afifi@gmail.com>
2021-07-09 00:54:35 +0100

    Finished core concept elements of the app

--- quizz_app/lib/main.dart
+++ quizz_app/lib/main.dart
@@ -16,20 +16,45 @@ class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
       'questionText': 'What\'s your favorite color?',
-      'answers': ['Black', 'Red', 'Green', 'White'],
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 0},
+        {'text': 'White', 'score': 2}
+      ],
     },
     {
       'questionText': 'What\'s your favorite animal?',
-      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+      'answers': [
+        {'text': 'Squirrel', 'score': 10},
+        {'text': 'Snake', 'score': 2},
+        {'text': 'Elephant', 'score': 0},
+        {'text': 'Lion', 'score': 8}
+      ],
     },
     {
       'questionText': 'Who\'s your favorite instructor?',
-      'answers': ['Max', 'Max', 'Max', 'Max'],
+      'answers': [
+        {'text': 'BEBOU', 'score': 10},
+        {'text': 'BEBOU', 'score': 10},
+        {'text': 'BEBOU', 'score': 10},
+        {'text': 'BEBOU', 'score': 10}
+      ],
     },
   ];
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -55,7 +80,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
--- quizz_app/lib/result.dart
+++ quizz_app/lib/result.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase {
+    var resultText = 'You did it!';
+    if (resultScore <= 8) {
+      resultText = 'You are still a little piece of cuteness';
+    }
+    else if (resultScore <= 18)
+    {
+      resultText = 'Not too shaby, I love you !';
+    }
+    else if (resultScore == 30)
+    {
+      resultText = 'You are amazing I LOVE YOU !';
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You did it!'),
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          TextButton(
+            child: Text('Restart Quiz'),
+            onPressed: resetHandler,
+          ),
+        ],
+      ),
     );
   }
 }
